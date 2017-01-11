f6ab44929c3234a391eb7a9373169604cd79e3e4
masayuki054 <msyk054@gmail.com>
2017-01-11 14:50:07 +0900

    2016.winter 12.prep

---
+++ org/Rakefile.setup.rb
@@ -0,0 +1,33 @@
+
+EXCLUDES = [
+  "Rakefile.org",
+  "Makefile.org",
+  "contents.org",
+  "links.org",
+  "log.org"
+]
+
+ORGS = [
+  "index.org", 
+  "firefox.org",
+  "google.org",
+  "guidance.org",
+  "pc.org",
+  "s.org",
+  "text.org",
+  "typing.org",
+  "web.org",
+  "windows.org",
+]
+
+SUBDIRS = [
+  "pc",
+  "text",
+  "typing",
+  "web",
+  "windows",
+]
+
+HTMLS = ORGS.map do |f|
+  File.basename(f,'.org')+".html"
+end
